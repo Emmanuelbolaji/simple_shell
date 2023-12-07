69e4f22ae0e65561bd90be33c9621e91083e6d52
Emmanuelbolaji <osundiranemmanuel@gmail.com>
2023-12-07 07:15:46 -0800

    shell

---
+++ betty.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int x = 56;
+	int y = 65;
+
+	int sum = x + y;
+
+	printf("sum = %d\n", sum);
+
+	return (0);
+}
